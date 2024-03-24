90f126057f3b5a47dada2ad1d79890b75296f7b7
ShibinRasheed <shibinrasheed333@gmail.com>
2024-03-24 16:41:32 +0530

    Started setting up mongoose

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"))
+
+
+
 app.get("*",(req,res)=>{
-  res.send("Hello There");
+  res.render("home");
 })
 
 app.listen(47, ()=>{
   console.log("App is listening")
 })
+
+
+
