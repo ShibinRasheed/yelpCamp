057dc88f2d08950774d27721d3dcd53510a86c83
ShibinRasheed <shibinrasheed333@gmail.com>
2024-03-27 23:53:32 +0530

    Setting up db

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const {places, descriptors} = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp",{
   useNewUrlParser :true,
@@ -11,3 +13,12 @@ db.on("error", console.error.bind(console,"connection error: "));
 db.once("open",()=>{
   console.log("Database connected");
 });
+
+
+const seedDB = async () => {
+   await Campground.deleteMany({});
+   const c = new Campground({title : "purple field"});
+   await c.save();
+}
+
+seedDB();
