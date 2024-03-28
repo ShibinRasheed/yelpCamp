1c0deb8a89080e0d5f327d56f79ae49f7efa8438
ShibinRasheed <shibinrasheed333@gmail.com>
2024-03-28 16:57:41 +0530

    Basic seed setup done

--- seeds/index.js
+++ seeds/index.js
@@ -3,10 +3,7 @@ const Campground = require("../models/campground");
 const cities = require("./cities");
 const {places, descriptors} = require("./seedHelpers");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp",{
-  useNewUrlParser :true,
-  useUnifiedTopology : true,
-})
+mongoose.connect("mongodb://localhost:27017/yelp-camp")
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console,"connection error: "));
@@ -15,10 +12,22 @@ db.once("open",()=>{
 });
 
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
    await Campground.deleteMany({});
-   const c = new Campground({title : "purple field"});
-   await c.save();
+   for(let i = 0 ; i < 50; i++)
+   {
+      let random1000 = Math.floor(Math.random()*1000);
+     const camp = new Campground({
+      location : `${cities[random1000].city}, ${cities[random1000].state}`,
+      title : `${sample(descriptors)} ${sample(places)}`
+     });
+     await camp.save();
+   }
 }
 
-seedDB();
+seedDB()
+  .then( () =>{
+    mongoose.connection.close();
+  })
