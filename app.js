b66310b1244f7d300ccb49c071ae7b8c75f82ae9
ShibinRasheed <shibinrasheed333@gmail.com>
2024-04-02 22:00:02 +0530

    Started Basic error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
 
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
@@ -31,36 +32,40 @@ app.get("/campgrounds/new", (req,res) =>{
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req,res)=>{
+app.post("/campgrounds", catchAsync(async (req,res)=>{
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.get("/campgrounds/:id", async (req,res)=>{
+app.get("/campgrounds/:id", catchAsync(async (req,res)=>{
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show",{campground});
-});
+}));
 
-app.get("/campgrounds/:id/edit", async (req,res)=>{
+app.get("/campgrounds/:id/edit",catchAsync(async (req,res)=>{
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/edit",{campground});
-});
+}));
 
-app.put("/campgrounds/:id", async (req,res)=>{
+app.put("/campgrounds/:id", catchAsync(async (req,res)=>{
   const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
   res.redirect(`/campgrounds/${req.params.id }`);
-});
+}));
 
-app.delete("/campgrounds/:id", async (req,res)=>{
+app.delete("/campgrounds/:id", catchAsync(async (req,res)=>{
   const {id} = req.params;
   const campground = await Campground.findByIdAndDelete(id);
   res.redirect(`/campgrounds`);
-});
+}));
 
 app.get("/",(req,res)=>{
   res.render("home");
 });
+
+app.use((err,req,res,next)=>{
+  res.send("Something went wrong");
+})
 
 app.listen(47, ()=>{
   console.log("App is listening");
