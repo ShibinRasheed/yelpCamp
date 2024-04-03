e49b57604844f66cbd93638be0b1a1f4e8a1c392
ShibinRasheed <shibinrasheed333@gmail.com>
2024-04-03 22:09:05 +0530

    Converted validation into a function to easily be applied on all the necessary routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
+const {campgroundSchema} = require("./views/schemas");
 
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
@@ -25,6 +25,16 @@ app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 
+const validateCampground = (req,res,next) =>{
+  const {error} = campgroundSchema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(msg,400);  
+  }else{
+    next();
+  }
+}
+
 app.get("/campgrounds", catchAsync(async (req,res)=>{
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index",{campgrounds});
@@ -34,22 +44,7 @@ app.get("/campgrounds/new", (req,res) =>{
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", catchAsync(async (req,res)=>{
-  
-  const campgroundSchema = Joi.object({
-    campground: Joi.object({
-      title : Joi.string().required(),
-      price : Joi.number().required().min(0),
-      image : Joi.string().required(),
-      location : Joi.string().required(),
-      description : Joi.string().required()
-    }).required()
-  })
-  const {error} = campgroundSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(",");
-    throw new ExpressError(msg,400);  
-  }
+app.post("/campgrounds", validateCampground, catchAsync(async (req,res)=>{
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
@@ -60,7 +55,7 @@ app.get("/campgrounds/:id", catchAsync(async (req,res)=>{
   res.render("campgrounds/show",{campground});
 }));
 
-app.get("/campgrounds/:id/edit",catchAsync(async (req,res)=>{
+app.get("/campgrounds/:id/edit",validateCampground, catchAsync(async (req,res)=>{
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/edit",{campground});
 }));
