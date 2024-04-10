aa96cb2ddf771a5746c596ea8b217c5464f476f3
ShibinRasheed <shibinrasheed333@gmail.com>
2024-04-10 22:55:40 +0530

    made Minor improvements to authentication system

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,22 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const passport = require('passport');
+const {storeReturnTo} = require("../views/middleware");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 })
 
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res,next) => {
   try {
     const { email, username, password } = req.body;
     const user = new User({ username, email });
-    const registerUser = await User.register(user, password);
-    req.flash("success", "You are registered");
-    res.redirect("/campgrounds");
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err=>{
+      if(err) return next(err);
+      req.flash("success", "You are registered");
+      res.redirect("/campgrounds");
+    })
   } catch (err) {
     req.flash("error", err.message);
     res.redirect("register");
@@ -25,9 +29,10 @@ router.get("/login", (req, res) => {
   res.render("users/login");
 })
 
-router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+router.post("/login",storeReturnTo, passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
   req.flash("success", "Welcome Back!");
-  res.redirect("/campgrounds");
+  const redirectUrl = res.locals.returnTo || "/campgrounds";
+  res.redirect(redirectUrl);
 })
 
 router.get("/logout", (req, res, next) => {
--- views/middleware.js
+++ views/middleware.js
@@ -1,7 +1,15 @@
-module.exports.isLoggedIn =  (req,res,next) =>{
-  if(!req.isAuthenticated()) {
-    req.flash("error","You must login to view the page.");
-    return res.redirect("/login");
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
+    req.flash('error', 'You must login to view the page.');
+    return res.redirect('/login');
   }
   next();
-}
+};
+
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
