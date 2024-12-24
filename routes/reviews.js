b101ffd57563b4081b551c5f8b62ec11de50cae9
ShibinRasheed <shibinrasheed333@gmail.com>
2024-12-24 14:53:35 +0530

    Completed Authorization

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,11 @@ const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
-const { isLoggedIn, validateReview } = require("../views/middleware");
+const {
+  isLoggedIn,
+  validateReview,
+  isReviewAuthor,
+} = require("../views/middleware");
 
 router.post(
   "/",
@@ -15,6 +19,7 @@ router.post(
     const { id } = req.params;
     const campground = await Campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -26,6 +31,7 @@ router.post(
 router.delete(
   "/:reviewId",
   isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
